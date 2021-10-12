3693c2f61a06d7123d3a4cc1dee5a69e7152c8b7
Your Name <you@example.com>
2021-10-12 08:13:31 -0500

    loops exercise

---
+++ script.js
@@ -0,0 +1,30 @@
+// 1
+for (i = 1; i <= 7; i++)
+console.log(i);
+
+// 2
+for (i = 5; i <= 25; i += 4)
+console.log(i);
+
+// 3a
+const wizards = [
+    "Harry Potter",
+    "Hermione Granger",
+    "Ron Weasley"
+]
+
+// 3b
+for (item of wizards){
+    console.log(item);
+}
+
+// 4a
+let harryPotterMovies = 0;
+
+// 4b
+while (harryPotterMovies <= 8) {
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+}
+
+
